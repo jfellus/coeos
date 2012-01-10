29b3594d61d2393841dfd0c1fbb2c595dca828ef
arnablan <arnablan@c4385178-6d17-0410-b40a-c157e05cd323>
2012-01-10 15:12:55 +0000

    - Compilation systematique de la version debug de coeos - Correction bug ?? Utilisation de libscript_symb pluto que libscript
    git-svn-id: svn+ssh://cvs-etis.ensea.fr/svn/SimulateurV1/coeos/coeos_7.0@270 c4385178-6d17-0410-b40a-c157e05cd323

--- Create_Coeos_Makefile.sh
+++ Create_Coeos_Makefile.sh
@@ -15,73 +15,95 @@
 ####################################################
 source ../scripts/COMPILE_FLAG
 
-# Nom du programme
-PROG_NAME="coeos"
+
 
 #DIVERS
 CC="gcc"
 SYSTEM=`uname`
 PWD=`pwd`
 
-
 GTKINCLUDES="`pkg-config --cflags gtk+-2.0 gthread-2.0`"
 GTKLIB=`pkg-config --libs gtk+-2.0 gthread-2.0`
 
-# Initialisation des libs, includes et flags
-LIBS="$GTKLIB -lmxml"
-INCLUDES="$GTKINCLUDES -I$PWD/include/leto -I$SIMULATOR_PATH/shared/include -I$PWD/include -I$PWD/include/shared -I."
-CFLAGS="-g3 -Wall -pedantic -D_REENTRANT -D_GNU_SOURCE -DDAEMON_COM -D`uname` -Wno-variadic-macros"
+ALL_CONFIGURATIONS=(debug release)
 
-#Version finale des libs, includes et flags
-FINALINCLUDES="$INCLUDES"
-FINALLIBS="$LIBS -L$SCRIPTLIBPATH $SCRIPTLIB -L$GRAPHICLIBPATH -l$GRAPHICLIB"
-FINALCFLAGS="$CFLAGS -DSYMBOLIQUE_VERSION"
+CFLAGS="-Wall -pedantic -D_REENTRANT -D_GNU_SOURCE -DDAEMON_COM -D`uname` -Wno-variadic-macros -DSYMBOLIQUE_VERSION"
 
-OBJDIR=$OBJPATH/$PROGNAME
-mkdir -p $OBJDIR
+MAKEFILE="Makefile.coeos"
+	
+#ecrasement du Makefile precedent
+	echo "" > $MAKEFILE
+#regle par defaut
+	echo "default: coeos coeos_debug" >> $MAKEFILE
+	echo "" >> $MAKEFILE
 
+
+for CONFIGURATION in ${ALL_CONFIGURATIONS[@]}
+do
+
+# Nom du programme
+
+	if [ $CONFIGURATION == "debug" ]
+	then
+		FINALCFLAGS="$CFLAGS -g"
+		POSTFIX="_debug"
+		
+	else
+		POSTFIX=""
+		FINALCFLAGS="$CFLAGS -g3"	
+	fi
+	PROG_NAME="coeos$POSTFIX"
+	
+	# Initialisation des libs, includes et flags
+	LIBS="$GTKLIB -lmxml"
+	INCLUDES="$GTKINCLUDES -I$PWD/include/leto -I$SIMULATOR_PATH/shared/include -I$PWD/include -I$PWD/include/shared -I."
+
+	#Version finale des libs, includes et flags
+	FINALINCLUDES="$INCLUDES"
+	FINALLIBS="$LIBS -L$SCRIPTLIBPATH -lscript_symb -L$GRAPHICLIBPATH -l${GRAPHICLIB}"
+	
+	OBJDIR=$OBJPATH/$PROG_NAME
+	
+	mkdir -p $OBJDIR
+	
 #le fichier xml pour le menu de leto
-XML="./xml/menu_leto_coeos.xml"
-
+	XML="./xml/menu_leto_coeos.xml"
+	
 #Gestion des fichiers a compiler
-SOURCES=`find src/ -depth -type f -name '*.c' | sed -e '/cc_leto.c/d'`
-OBJECTS=""
-
+	SOURCES=`find src/ -depth -type f -name '*.c' | sed -e '/cc_leto.c/d'`
+	OBJECTS=""
+	
 ####################################################
 #Creation du Makefile
 ####################################################
+	
 
-MAKEFILE="Makefile.$PROG_NAME"
-
-#ecrasement du Makefile precedent
-echo "" > $MAKEFILE
-#regle par defaut
-echo "default: $PROG_NAME" >> $MAKEFILE
-echo "" >> $MAKEFILE
-
+	
 # creer les regles
 #pour chaque  .o
-for i in $SOURCES
-do
-  echo "processing '$i'"
-  FICHIER=`basename $i .c`
-  CHEMIN=`echo $i | sed -e s@$FICHIER.c@@`
-  echo "$OBJDIR/$FICHIER.o:$i" >> $MAKEFILE
-  echo -e "\t@echo \"[processing $i...]\"">> $MAKEFILE
-  echo -e "\t@(cd $CHEMIN; $CC $FINALCFLAGS $FINALINCLUDES -c -o $OBJDIR/$FICHIER.o $FICHIER.c)" >> $MAKEFILE
-  echo "" >> $MAKEFILE
-  OBJECTS="$OBJECTS $OBJDIR/$FICHIER.o"
-done
-
+	for i in $SOURCES
+	do
+	  echo "processing '$i'"
+	  FICHIER=`basename $i .c`
+	  CHEMIN=`echo $i | sed -e s@$FICHIER.c@@`
+	  echo "$OBJDIR/$FICHIER.o:$i" >> $MAKEFILE
+	  echo -e "\t@echo \"[processing $i...]\"">> $MAKEFILE
+	  echo -e "\t@(cd $CHEMIN; $CC $FINALCFLAGS $FINALINCLUDES -c -o $OBJDIR/$FICHIER.o $FICHIER.c)" >> $MAKEFILE
+	  echo "" >> $MAKEFILE
+	  OBJECTS="$OBJECTS $OBJDIR/$FICHIER.o"
+	done
+	
 #pour l'edition de liens et le lien sur le binaire
-echo "$PROG_NAME: $OBJECTS" >> $MAKEFILE
-echo -e "\t@mkdir -p $BINDIR" >> $MAKEFILE
-echo -e "\t@echo \"[Link objects...] \"">> $MAKEFILE
-echo -e "\t@$CC $OBJECTS -o $BINDIR/$PROG_NAME $FINALLIBS " >> $MAKEFILE
-echo -e "\t@cp $XML  $DIR_BIN_LETO_PROM/. " >> $MAKEFILE
-echo -e "\t@cp $BINDIR/$PROG_NAME $DIR_BIN_LETO_PROM " >> $MAKEFILE
-echo -e "\t@cp $BINDIR/$PROG_NAME $DIR_BIN_LETO_PROM " >> $MAKEFILE
-echo "" >> $MAKEFILE
+	echo "$PROG_NAME: $OBJECTS" >> $MAKEFILE
+	echo -e "\t@mkdir -p $BINDIR" >> $MAKEFILE
+	echo -e "\t@echo \"[Link objects...] \"">> $MAKEFILE
+	echo -e "\t@$CC $OBJECTS -o $BINDIR/$PROG_NAME $FINALLIBS " >> $MAKEFILE
+	echo -e "\t@cp $XML  $DIR_BIN_LETO_PROM/. " >> $MAKEFILE
+	echo -e "\t@cp $BINDIR/$PROG_NAME $DIR_BIN_LETO_PROM " >> $MAKEFILE
+	echo -e "\t@cp $BINDIR/$PROG_NAME $DIR_BIN_LETO_PROM " >> $MAKEFILE
+	echo "" >> $MAKEFILE
+	
+done
 
 #regles additionnelles
 echo "clean:" >> $MAKEFILE
