38d5a8844f9af35fea1250029ffda903b319843f
souhhano <souhhano@c4385178-6d17-0410-b40a-c157e05cd323>
2012-09-13 16:12:22 +0000

    rajout de l edition des liens des librairies dynamique dans leto, sleto et ccleto
    git-svn-id: svn+ssh://cvs-etis.ensea.fr/svn/SimulateurV1/coeos/trunk@302 c4385178-6d17-0410-b40a-c157e05cd323

--- Create_Sleto_Makefile.sh
+++ Create_Sleto_Makefile.sh
@@ -74,7 +74,7 @@ done
 echo "$PROG_NAME: $OBJECTS" >> $MAKEFILE
 echo -e "\t@mkdir -p $BINDIR" >> $MAKEFILE
 echo -e "\t@echo \"[Link objects...] \"">> $MAKEFILE
-echo -e "\t@$CC $OBJECTS -o $BINDIR/$PROG_NAME $FINALLIBS " >> $MAKEFILE
+echo -e "\t@$CC $OBJECTS -o $BINDIR/$PROG_NAME $FINALLIBS -rdynamic" >> $MAKEFILE
 echo -e "\t@cp $XML $DIR_BIN_LETO_PROM " >> $MAKEFILE
 echo -e "\t@cp $BINDIR/$PROG_NAME $DIR_BIN_LETO_PROM " >> $MAKEFILE
 echo -e "\t@cp $BINDIR/$PROG_NAME $DIR_BIN_LETO_PROM " >> $MAKEFILE
